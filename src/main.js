25932cb051317ce9dc5e6f4d10c2a0ad8789ce8c
Mirzet Kameric <mirzetkameric@hotmail.com>
2021-03-07 14:38:56 +0100

    changes after meeting

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const quiz = {
+    answeredQuestions: 0,
+    correctAnswers: 0,
+    activeQuestion: 1,
+}
+
+function renderQuestion (activeQuestion) {
+    // renderQuestionAndAnswer
+    // renderStats
+}
+
+function addListeners () {
+
+}
+
+function answerQuestion () {
+    // process the answer if it's correct or not and add class to body depending on correct/not
+    // display button for next question and add listener
+    // go to next question and render it
+
+    // A user can see the correct answer for questions
+    // Total score view after questions are finished
+}
+
+
+// A user can "cheat" to see the correct answer, this forfeits the question
+// A user has access to resources for further study on each question
