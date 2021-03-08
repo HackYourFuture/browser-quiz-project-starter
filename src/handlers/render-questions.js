959d1679ad8d3cab52b58c098e078b9671287636
Cemal <sph_cml1974@outlook.com>
2021-03-08 20:29:17 +0100

    render-question.js does not import quizData anymore

--- src/handlers/render-questions.js
+++ src/handlers/render-questions.js
@@ -1,10 +1,11 @@
-import { quizData } from '../data.js';
+
+
 import { createQuestionList } from '../views/questions-list.js';
 
 export function renderQuestions() {
-    for (let i = 0; i < quizData.questions.length; i++) {
-      createQuestionList().innerHTML = quizData.questions[i].text;
-    }
-  }
+    const list = createQuestionList()
+    document.body.appendChild(list);
+}
+
 
 
--- src/views/questions-list.js
+++ src/views/questions-list.js
@@ -1,10 +1,17 @@
 import { quizData } from "../data.js";
+
 export function createQuestionList() {
   const list = document.createElement('ul');
-  document.body.appendChild(list);
-  const fakeLi = document.createElement('li');
-  list.appendChild(fakeLi);
-  console.log(quizData.questions);
+  
+    quizData.questions.forEach(item =>{
+        const questionsLi = document.createElement('li')
+        list.appendChild(questionsLi)
+        questionsLi.textContent = item.text
+    // console.log(item.text)
+})
+  
+    
+  
   // TODO: actually put things into the list
   return list;
 }
