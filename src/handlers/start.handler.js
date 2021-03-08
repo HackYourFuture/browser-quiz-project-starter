a185dcbb8e1ec0c622079b230bd4633d36ff0ebd
Mahmood <pejvakco@gmail.com>
2021-03-08 11:34:07 +0100

    Delete start handler and listener

--- src/handlers/start.handler.js
+++
@@ -1,8 +0,0 @@
-export const startHandler = (event) => {
-  const startContainer = document.querySelector('.startContainer');
-  startContainer.remove();
-
-  const quizContainer = document.querySelector('.quizContainer');
-  //debugger;
-  quizContainer.classList.add('showQuiz');
-};
--- src/listeners/start.listener.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import { startHandler } from '../handlers/start.handler.js';
-
-const startButton = document.querySelector('#startButton');
-startButton.addEventListener('click', startHandler);
