c7acff7b45900b9faf58c9afdcd8ed5775f0cf0f
Cemal <sph_cml1974@outlook.com>
2021-03-08 10:26:04 +0100

    Took all the questions from data.js and showed them in the page.

--- src/handlers/render-questions.js
+++ src/handlers/render-questions.js
@@ -1,3 +1,25 @@
-export function renderQuestions(uiElement, questionsElement) {
-    uiElement.appendChild(questionsElement);
-}
+import { quizData } from "../data.js";
+
+export function renderQuestions() {
+    
+    const list = document.createElement('ul');
+    document.body.appendChild(list);
+    for (let i = 0; i < quizData.questions.length; i++){
+            const questionsLi = document.createElement('li');
+            list.appendChild(questionsLi);
+            questionsLi.innerHTML = quizData.questions[i].text;
+            console.log(quizData.questions[i].text);
+        }
+            
+        
+    
+        // TODO: actually put things into the list
+    
+        return list;
+    }
+
+    
+
+
+
+
--- src/views/questions-list.js
+++ src/views/questions-list.js
@@ -3,10 +3,16 @@ import { quizData } from "../data.js";
 export function createQuestionList() {
     const list = document.createElement('ul');
 
-    const fakeLi = document.createElement('li');
-    fakeLi.textContent = 'Hello world!';
-    list.appendChild(fakeLi);
-
+    
+    
+    
+    for (let i = 0; i < quizData.questions.length; i++){
+        const fakeLi = document.createElement('li');
+        list.appendChild(fakeLi);
+        fakeLi.innerHTML = quizData.questions[i].text;
+        console.log(quizData.questions.text)
+    }
+        
     console.log(quizData.questions);
 
     // TODO: actually put things into the list
