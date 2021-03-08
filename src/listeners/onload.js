01f16ed28a929a028274b4016b51ea61674368a8
Veronika <veroniqe@rambler.ru>
2021-03-08 20:59:35 +0100

    sandbox.html is deleted

--- src/listeners/onload.js
+++ src/listeners/onload.js
@@ -4,7 +4,6 @@ import { renderScore } from '../handlers/render-score.js';
 window.addEventListener('DOMContentLoaded', () => {
   const ui = document.getElementById('user-interface');
   const list = createQuestionList();
-
   renderQuestions(ui, list);
   renderScore();
 });
