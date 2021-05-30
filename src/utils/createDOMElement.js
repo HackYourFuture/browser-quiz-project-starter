d50f6b238ce743a23c27b2c767cc6703757647c9
Rohan Nicholls <rohan.nicholls@gmail.com>
2021-05-30 12:38:35 +0200

    A better createDomElement

--- src/utils/createDOMElement.js
+++ src/utils/createDOMElement.js
@@ -2,20 +2,40 @@
 
 /**
  * This function creates a DOM element with the given tag.
- * In the options parameter it is possible to add some extra data to it:
- * 
- *  id - the id of the element
+ * In the options parameter it is possible to add extra attributes
+ *
+ * @param {string|[string, Object, ...any]} element
+ * @return {HTMLElement|Text}
  */
-const createDOMElement = (tag, options) => {
-    const { id } = options || {};
+const createDOMElement = (element) => {
+  if (typeof element === 'string') {
+    console.log('considered a string', element);
+    return document.createTextNode(element);
+  }
+  // create a tag and its children
+  console.log(element);
+  const [tag, options = {}, ...children] = element;
+  const node = document.createElement(tag);
+  // set the options as attributes in the new element
+  Object.entries(options).forEach(([key, val]) => node.setAttribute(key, val));
 
-    const element = document.createElement(tag);
-    
-    if (id != null) {
-        element.id = id;
-    }
+  children.forEach((child) => node.appendChild(createDOMElement(child)));
 
-    return element;
-}
+  return node;
+};
 
 export default createDOMElement;
+
+const test = createDOMElement([
+  'div',
+  { id: 'bubba' },
+  ['h1', {}, 'What is a node'],
+  [
+    'ol',
+    {},
+    ['li', { id: 'optionA' }, 'constant, let, variable'],
+    ['li', { id: 'optionB' }, 'var, const, let, function'],
+    ['li', { id: 'optionC' }, 'var, let, const'],
+  ],
+]);
+document.body.appendChild(test);
