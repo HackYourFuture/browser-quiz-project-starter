3dfaf41504aaced6a2f4d6d774c3187b8690ff51
Mohammad <malnajjar16@gmail.com>
2021-05-31 13:57:44 +0200

    select only oneAnswer/know which answer isCorrect

--- src/views/createQuestionElement.js
+++ src/views/createQuestionElement.js
@@ -1,31 +1,58 @@
 'use strict';
 
-import createDOMElement from "../utils/createDOMElement.js";
+import createDOMElement from '../utils/createDOMElement.js';
 
 export const createQuestionElement = (question) => {
-    const container = createDOMElement('div');
-    const title = createDOMElement('h1');
-    title.innerText = question.text;
-    container.appendChild(title);
+  const container = createDOMElement('div');
+  const title = createDOMElement('h1');
+  title.innerText = question.text;
+  container.appendChild(title);
+  const answerContainer = createDOMElement('ol');
+  let clickCounter = 0;
+  for (const answerKey in question.answers) {
+    const answer = createAnswerElement(answerKey, question.answers[answerKey]);
 
-    const answerContainer = createDOMElement('ol');
+    answer.addEventListener('click', (e) => {
+      console.log(e);
+      clickCounter++;
 
-    for (const answerKey in question.answers) {
-        const answer = createAnswerElement(question.answers[answerKey]);
-        answer.addEventListener('click', (e)=>{console.log(e.target)})
-        answerContainer.appendChild(answer);
-    }
+      if (
+        e.target.dataset.answerItemKey == question.correct &&
+        clickCounter == 1
+      ) {
+        e.target.classList.add('correct');
+      } else if (
+        e.target.dataset.answerItemKey != question.correct &&
+        clickCounter == 1
+      ) {
+        e.target.parentElement.childNodes.forEach((item) => {
+          if (item.dataset.answerItemKey == question.correct) {
+            item.classList.add('correct');
+          } else {
+            e.target.classList.add('wrong');
+          }
+        });
+      } else {
+        e != e.target; // we will discuss this line later with Arsalan;
+        title.innerText = 'Click next Button';
+        title.style.color = 'red';
+        title.style.width = '800px';
+      }
+    });
 
-    container.appendChild(answerContainer);
-    
-    return container;
-}
+    answerContainer.appendChild(answer);
+  }
+  container.appendChild(answerContainer);
 
-export const createAnswerElement = (answerText) => {
-    const answerElement = createDOMElement('li',{className: 'btn'});
-    answerElement.innerText = answerText;
+  return container;
+};
 
-    return answerElement;
-}
+export const createAnswerElement = (answerKey, answerText) => {
+  const answerElement = createDOMElement('li', { className: 'btn' });
+  answerElement.innerText = answerText;
+  answerElement.dataset.answerItemKey = answerKey;
+  answerElement.classList.add('answersStyle');
+  return answerElement;
+};
 
 export default createQuestionElement;
