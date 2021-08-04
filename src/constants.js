439ea586a43bb2b1c04755e2cdb3fb78822c3adc
Reshad Majidi <84543794+majidireshad@users.noreply.github.com>
2021-08-04 20:06:19 +0200

    Added restart button to the last question (#10)
    * saving work
    
    * Seperating questions added to another branch
    
    * saving work
    
    * added restart button with the last question
    
    * corrected the restart button creation setup
    
    Co-authored-by: Reshad <rmajidi09@gmail.com>

--- src/constants.js
+++ src/constants.js
@@ -10,3 +10,4 @@
 export const QUESTION_CONTAINER_ID = 'question-container';
 export const QUIZ_CONTAINER_ID = 'quiz-container';
 export const NEXT_QUESTION_BUTTON_ID = 'next-question-button';
+export const LAST_QUESTION_BUTTON_ID = 'last-question-button';
--- src/handlers/questionHandlers.js
+++ src/handlers/questionHandlers.js
@@ -1,22 +1,58 @@
-'use strict';
+  'use strict';
 
-import { QUESTION_CONTAINER_ID } from '../constants.js';
-import { createQuestionElement } from '../views/questionViews.js';
-import { clearDOMElement, getDOMElement } from '../utils/DOMUtils.js';
-import { quizData } from '../data.js';
+  import {QUESTION_CONTAINER_ID} from '../constants.js';
+  import {createQuestionElement} from '../views/questionViews.js';
+  import {clearDOMElement, createDOMElement, getDOMElement} from '../utils/DOMUtils.js';
+  import {quizData} from '../data.js';
+  import {LAST_QUESTION_BUTTON_ID, NEXT_QUESTION_BUTTON_ID} from '../constants.js';
 
-export const showCurrentQuestion = () => {
+
+  export const showCurrentQuestion = () => {
+    
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
 
   const questionDOM = createQuestionElement(currentQuestion);
-
   const questionContainer = getDOMElement(QUESTION_CONTAINER_ID);
+
   clearDOMElement(questionContainer);
   questionContainer.appendChild(questionDOM);
-};
+  if (quizIsEnded() === true) {
+    const nextQuestionBtn = getDOMElement(NEXT_QUESTION_BUTTON_ID);
+    const lastQuestionBtn = getDOMElement(LAST_QUESTION_BUTTON_ID);
+    nextQuestionBtn.style.visibility = 'hidden';
+    lastQuestionBtn.style.visibility = 'visible';
+  } else {
+    restartBtnSetup();
+  };
+  }
 
-export const handleNextQuestion = () => {
+  const {questions} = quizData;
+  const lengthOfArray = questions.length - 1;
+
+
+  export const handleNextQuestion = () => {
+  if (quizIsEnded() === true) {
+    quizData.currentQuestionIndex = 0;
+    const nextQuestionBtn = getDOMElement(NEXT_QUESTION_BUTTON_ID);
+    const lastQuestionBtn = getDOMElement(LAST_QUESTION_BUTTON_ID);
+    nextQuestionBtn.style.visibility = 'hidden';
+    lastQuestionBtn.style.visibility = 'visible';
+  } else {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
-
+  };
   showCurrentQuestion();
-};
+  };
+
+  export const quizIsEnded = () => {
+  return quizData.currentQuestionIndex >= lengthOfArray;
+  };
+
+/**
+ * The restartBtnSetup function resets the buttons when the test is restarted.
+ */
+  const restartBtnSetup = () => {
+    const nextQuestionBtn = getDOMElement(NEXT_QUESTION_BUTTON_ID);
+    const lastQuestionBtn = getDOMElement(LAST_QUESTION_BUTTON_ID);
+    nextQuestionBtn.style.visibility = 'visible';
+    lastQuestionBtn.style.visibility = 'hidden';
+  };
