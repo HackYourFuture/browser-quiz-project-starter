8816f6d211dfb636520c8da09cd2f41674a1be6e
Obada <obadamoustafa@gmail.com>
2021-08-04 17:26:07 +0200

    add unique id for every button answer

--- src/views/questionViews.js
+++ src/views/questionViews.js
@@ -2,15 +2,17 @@
 
 import { NEXT_QUESTION_BUTTON_ID } from '../constants.js';
 import { nextQuestion } from '../listeners/questionListeners.js';
-import { createDOMElement } from '../utils/DOMUtils.js';
+import { createDOMElement, getDOMElement } from '../utils/DOMUtils.js';
 
 /**
  * Create an Answer element
  */
 export const createAnswerElement = (answerText) => {
   const answerElement = createDOMElement('li');
-  answerElement.innerText = answerText;
-
+  const answerButton = createDOMElement("button");
+  answerElement.appendChild(answerButton);
+  answerButton.innerText = answerText;
+  
   return answerElement;
 };
 
@@ -18,20 +20,32 @@ export const createAnswerElement = (answerText) => {
  * Create a full question element
  */
 export const createQuestionElement = (question) => {
+  
   const container = createDOMElement('div');
   const title = createDOMElement('h1');
   title.innerText = question.text;
   container.appendChild(title);
 
   const answerContainer = createDOMElement('ol');
+ 
 
   for (const answerKey in question.answers) {
     const answer = createAnswerElement(question.answers[answerKey]);
     answerContainer.appendChild(answer);
   }
 
+  // here we put a unique Id for every button
+  let answerKeyNumber = 0;
+  let buttonsEl = answerContainer.querySelectorAll("button");
+  for (const answerKey in question.answers) {
+    buttonsEl[answerKeyNumber].id = answerKey;
+    answerKeyNumber++
+  }
+  
+  //!important
+  //buttonsEl.addEventListener("click", checkAnswer); we need to check later
   container.appendChild(answerContainer);
-
+  
   return container;
 };
 
