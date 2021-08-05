ce6edc72487ea33a5fa2c24cd8d8a946b64071aa
Claudia de Windt <caucrah@gmail.com>
2021-08-05 17:08:39 +0200

    Added the score to the checkAnswer function

--- src/views/answer-selection.js
+++ src/views/answer-selection.js
@@ -1,11 +1,18 @@
-import {quizData} from '../data.js'
- 
- export  const checkAnswer = function selectedAnswer(e) {
-  //const buttonTarget = e.target.id;
-  console.log(this.id);
-  if (this.id === quizData.questions[quizData.currentQuestionIndex].correct){
-    this.classList.add("correct-answer");
+import { quizData } from '../data.js';
+import { getDOMElement } from '../utils/DOMUtils.js';
+
+const spanEl = getDOMElement('score');
+console.log(spanEl);
+spanEl.textContent = 0;
+
+export const checkAnswer = function selectedAnswer(e) {
+  if (this.id === quizData.questions[quizData.currentQuestionIndex].correct) {
+    this.classList.add('correct-answer');
+
+    return (spanEl.textContent = parseFloat(spanEl.textContent) + 100);
   } else {
-    this.classList.add("wrong-answer");
+    this.classList.add('wrong-answer');
+
+    return (spanEl.textContent = parseFloat(spanEl.textContent) - 50);
   }
 };
