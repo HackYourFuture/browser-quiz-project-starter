ad66ff528daff4d042f6611cd1efc2c707b7cb3f
Obada <obadamoustafa@gmail.com>
2021-08-05 18:32:47 +0200

    now we can select one answer and put the score

--- src/views/answer-selection.js
+++ src/views/answer-selection.js
@@ -6,16 +6,19 @@ spanEl.textContent = 0;
 
 export const checkAnswer = function selectedAnswer(e) {
   const buttonsCon = this.parentElement.parentElement;
-  console.log(buttonsCon);
   const answerButtons = buttonsCon.querySelectorAll("button");
-  console.log(answerButtons);
   if (this.id === quizData.questions[quizData.currentQuestionIndex].correct) {
     this.classList.add('correct-answer');
     answerButtons.forEach( button => button.removeEventListener('click',checkAnswer));
     return (spanEl.textContent = parseFloat(spanEl.textContent) + 100);
   } else {
     this.classList.add('wrong-answer');
-    answerButtons.forEach( button => button.removeEventListener('click',checkAnswer));
+    const correct = quizData.questions[quizData.currentQuestionIndex].correct;
+
+    /* forEach button if it's id = the correct answer letter (ex: "c") it give correct-answer class to the button
+    and in all situations it removes the Event listener.*/
+
+    answerButtons.forEach(element => element.id == correct ? element.classList.add("correct-answer") && element.removeEventListener('click',checkAnswer): element.removeEventListener('click',checkAnswer) );
     return (spanEl.textContent = parseFloat(spanEl.textContent) - 50);
   }
 };
