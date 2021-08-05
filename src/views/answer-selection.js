ec7b60f297f3710126474711682fd7aeb3d812b8
Obada <obadamoustafa@gmail.com>
2021-08-05 17:33:43 +0200

    user inly can select one answer

--- src/views/answer-selection.js
+++ src/views/answer-selection.js
@@ -2,17 +2,20 @@ import { quizData } from '../data.js';
 import { getDOMElement } from '../utils/DOMUtils.js';
 
 const spanEl = getDOMElement('score');
-console.log(spanEl);
 spanEl.textContent = 0;
 
 export const checkAnswer = function selectedAnswer(e) {
+  const buttonsCon = this.parentElement.parentElement;
+  console.log(buttonsCon);
+  const answerButtons = buttonsCon.querySelectorAll("button");
+  console.log(answerButtons);
   if (this.id === quizData.questions[quizData.currentQuestionIndex].correct) {
     this.classList.add('correct-answer');
-
+    answerButtons.forEach( button => button.removeEventListener('click',checkAnswer));
     return (spanEl.textContent = parseFloat(spanEl.textContent) + 100);
   } else {
     this.classList.add('wrong-answer');
-
+    answerButtons.forEach( button => button.removeEventListener('click',checkAnswer));
     return (spanEl.textContent = parseFloat(spanEl.textContent) - 50);
   }
 };
