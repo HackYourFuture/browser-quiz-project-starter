2ade96538a85c60d4c7f91b286506526f8ba5d5f
Obada <obadamoustafa@gmail.com>
2021-08-05 12:18:06 +0200

    put the functions inside a separated file

--- src/views/answer-selection.js
+++ src/views/answer-selection.js
@@ -1,5 +1,11 @@
-
-export  const checkAnswer = function selectedAnswer() {
-    //console.log(e);
-    console.log("Hi I am here")
-  };
+import {quizData} from '../data.js'
+ 
+ export  const checkAnswer = function selectedAnswer(e) {
+  //const buttonTarget = e.target.id;
+  console.log(this.id);
+  if (this.id === quizData.questions[quizData.currentQuestionIndex].correct){
+    this.classList.add("correct-answer");
+  } else {
+    this.classList.add("wrong-answer");
+  }
+};
