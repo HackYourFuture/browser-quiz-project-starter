b40d4adb9e6a0e4adaf994ccf7a1ed7a556fd9e3
Reshad Majidi <84543794+majidireshad@users.noreply.github.com>
2021-08-08 08:35:59 +0200

    user can see the wrong and correct answer, score is added and shown immediately (#16)
    Co-authored-by: Reshad <rmajidi09@gmail.com>
    Co-authored-by: Collin Duncan <cgduncan7@users.noreply.github.com>

--- src/handlers/questionHandlers.js
+++ src/handlers/questionHandlers.js
@@ -1,71 +1,81 @@
-  'use strict';
+'use strict';
 
-  import {QUESTION_CONTAINER_ID} from '../constants.js';
-  import {createQuestionElement} from '../views/questionViews.js';
-  import {clearDOMElement, createDOMElement, getDOMElement} from '../utils/DOMUtils.js';
-  import {quizData} from '../data.js';
-  import {LAST_QUESTION_BUTTON_ID, NEXT_QUESTION_BUTTON_ID} from '../constants.js';
-
-
-  export const showCurrentQuestion = () => {
-    
-  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
-
-  const questionDOM = createQuestionElement(currentQuestion);
-  const questionContainer = getDOMElement(QUESTION_CONTAINER_ID);
-
-  clearDOMElement(questionContainer);
-  questionContainer.appendChild(questionDOM);
-  if (quizIsEnded() === true) {
-    const nextQuestionBtn = getDOMElement(NEXT_QUESTION_BUTTON_ID);
-    const lastQuestionBtn = getDOMElement(LAST_QUESTION_BUTTON_ID);
-    nextQuestionBtn.style.visibility = 'hidden';
-    lastQuestionBtn.style.visibility = 'visible';
-  } else {
-    restartBtnSetup();
-  };
-  }
-
-  const {questions} = quizData;
-  const lengthOfArray = questions.length - 1;
+import {QUESTION_CONTAINER_ID, ANSWER_CONTAINER_ID} from '../constants.js';
+import {createQuestionElement} from '../views/questionViews.js';
+import {clearDOMElement, getDOMElement} from '../utils/DOMUtils.js';
+import {quizData} from '../data.js';
+import {LAST_QUESTION_BUTTON_ID, NEXT_QUESTION_BUTTON_ID} from '../constants.js';
 
 
-  export const handleNextQuestion = () => {
-  if (quizIsEnded() === true) {
-    quizData.currentQuestionIndex = 0;
-    const nextQuestionBtn = getDOMElement(NEXT_QUESTION_BUTTON_ID);
-    const lastQuestionBtn = getDOMElement(LAST_QUESTION_BUTTON_ID);
-    nextQuestionBtn.style.visibility = 'hidden';
-    lastQuestionBtn.style.visibility = 'visible';
-  } else {
-  quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
-  };
-  showCurrentQuestion();
+export const showCurrentQuestion = () => {
+
+    const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+
+    const questionDOM = createQuestionElement(currentQuestion);
+    const questionContainer = getDOMElement(QUESTION_CONTAINER_ID);
+
+    clearDOMElement(questionContainer);
+    questionContainer.appendChild(questionDOM);
+    if (quizIsEnded() === true) {
+        const nextQuestionBtn = getDOMElement(NEXT_QUESTION_BUTTON_ID);
+        const lastQuestionBtn = getDOMElement(LAST_QUESTION_BUTTON_ID);
+        nextQuestionBtn.style.visibility = 'hidden';
+        lastQuestionBtn.style.visibility = 'visible';
+    } else if (quizData.currentQuestionIndex === 0) {
+        restartQuizSetup();
+    }
+};
+
+const {questions} = quizData;
+const lengthOfArray = questions.length - 1;
+
+
+export const handleNextQuestion = () => {
+    if (quizIsEnded() === true) {
+        quizData.currentQuestionIndex = 0;
+    } else {
+        quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+    }
+    showCurrentQuestion();
 };
 
 export const handleCheckAnswer = (selectedAnswer) => {
-const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
-let clickedAnswer = selectedAnswer.innerText
+    const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+    let clickedAnswer = selectedAnswer.innerText;
+    let currentCorrectAnswer = currentQuestion.answers[currentQuestion.correct];
 
-let currentCorrectAnswer = currentQuestion.answers[currentQuestion.correct];
 
-if(clickedAnswer === currentCorrectAnswer){
-  selectedAnswer.style.cssText = "background-color: green; color: white;"
-} else {
-selectedAnswer.style.cssText = "background-color: red; color: white;"
-}
-}
+    if (clickedAnswer === currentCorrectAnswer) {
+        selectedAnswer.classList.add('correct-answer');
+        quizData.correctAnswersCounter = quizData.correctAnswersCounter + 1;
+    } else {
+        selectedAnswer.classList.add('wrong-answer');
+        let correctAnswerElement = getDOMElement(currentQuestion.correct);
+        correctAnswerElement.classList.add('correct-answer');
+    }
+
+    document.getElementById(
+        'corrects'
+    ).innerText = `${quizData.correctAnswersCounter} Correct of ${lengthOfArray}`;
+
+    const answerElement = getDOMElement(ANSWER_CONTAINER_ID);
+    answerElement.style.pointerEvents = "none";
+};
 
 export const quizIsEnded = () => {
-  return quizData.currentQuestionIndex >= lengthOfArray;
-  };
+    return quizData.currentQuestionIndex >= lengthOfArray;
+};
 
 /**
  * The restartBtnSetup function resets the buttons when the test is restarted.
  */
-  const restartBtnSetup = () => {
+const restartQuizSetup = () => {
     const nextQuestionBtn = getDOMElement(NEXT_QUESTION_BUTTON_ID);
     const lastQuestionBtn = getDOMElement(LAST_QUESTION_BUTTON_ID);
     nextQuestionBtn.style.visibility = 'visible';
     lastQuestionBtn.style.visibility = 'hidden';
-  };
+    quizData.correctAnswersCounter = 0;
+    document.getElementById(
+        'corrects'
+    ).innerText = '';
+};
--- src/views/questionViews.js
+++ src/views/questionViews.js
@@ -1,20 +1,24 @@
 'use strict';
 
-import { NEXT_QUESTION_BUTTON_ID, LAST_QUESTION_BUTTON_ID  } from '../constants.js';
-import { nextQuestion , checkAnswer} from '../listeners/questionListeners.js';
-import { createDOMElement } from '../utils/DOMUtils.js';
+import {NEXT_QUESTION_BUTTON_ID, LAST_QUESTION_BUTTON_ID, ANSWER_CONTAINER_ID} from '../constants.js';
+import {nextQuestion, checkAnswer} from '../listeners/questionListeners.js';
+import {createDOMElement} from '../utils/DOMUtils.js';
 
 /**
  * Create an Answer element
  */
-export const createAnswerElement = (answerText) => {
-  const answerElement = createDOMElement('li');
+export const createAnswerElement = (answerText, elementID) => {
+  const answerElement = createDOMElement('li', {
+      id: elementID,
+  });
   answerElement.innerText = answerText;
   answerElement.setAttribute('class', 'answers-default')
   answerElement.style.cursor = "pointer";
-  answerElement.addEventListener("click", checkAnswer)
+  answerElement.addEventListener("click", checkAnswer, { passive: true })
+  
   return answerElement;
 };
+
 
 /**
  * Create a full question element
@@ -25,19 +29,19 @@ export const createQuestionElement = (question) => {
   title.innerText = question.text;
   title.setAttribute('class', 'question-title')
   container.appendChild(title);
-  
 
-  const answerContainer = createDOMElement('ol');
+    const answerContainer = createDOMElement('ol', {
+        id: ANSWER_CONTAINER_ID,
+    });
 
-  for (const answerKey in question.answers) {
-    const answer = createAnswerElement(question.answers[answerKey]);
-    answerContainer.appendChild(answer);
+    for (const answerKey in question.answers) {
+        const answer = createAnswerElement(question.answers[answerKey], answerKey);
+        answerContainer.appendChild(answer);
+    }
 
-  }
+    container.appendChild(answerContainer);
 
-  container.appendChild(answerContainer);
-
-  return container;
+    return container;
 };
 
 /**
@@ -45,14 +49,14 @@ export const createQuestionElement = (question) => {
  */
 
 export const createNextQuestionButtonElement = () => {
-  const buttonElement = createDOMElement('button', {
-    id: NEXT_QUESTION_BUTTON_ID,
-  });
+    const buttonElement = createDOMElement('button', {
+        id: NEXT_QUESTION_BUTTON_ID,
+    });
 
-  buttonElement.innerText = 'Next question';
-  buttonElement.addEventListener('click', nextQuestion);
-  return buttonElement;
-  
+    buttonElement.innerText = 'Next question';
+    buttonElement.addEventListener('click', nextQuestion);
+    return buttonElement;
+
 };
 
 /**
@@ -60,14 +64,12 @@ export const createNextQuestionButtonElement = () => {
  */
 
 export const createLastQuestionButtonElement = () => {
-  const buttonLastElement = createDOMElement('button', {
-    id: LAST_QUESTION_BUTTON_ID,
-  });
+    const buttonLastElement = createDOMElement('button', {
+        id: LAST_QUESTION_BUTTON_ID,
+    });
 
 
-  buttonLastElement.innerText = 'Restart Test';
-  buttonLastElement.addEventListener('click', nextQuestion);
-  return buttonLastElement;
+    buttonLastElement.innerText = 'Restart Test';
+    buttonLastElement.addEventListener('click', nextQuestion);
+    return buttonLastElement;
 };
-
-
