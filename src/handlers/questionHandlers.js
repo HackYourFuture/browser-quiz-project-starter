98836bf0541a6844cea94470acb57f2983549d89
SaidYein <saidyein@gmail.com>
2021-11-08 13:50:05 +0100

    attempt-1 to select the answer

--- src/handlers/questionHandlers.js
+++ src/handlers/questionHandlers.js
@@ -2,18 +2,35 @@
 
 import { QUESTION_CONTAINER_ID } from '../constants.js';
 import { createQuestionElement } from '../views/questionViews.js';
-import { clearDOMElement, getDOMElement } from '../utils/DOMUtils.js';
+import { clearDOMElement, getDOMElement, getKeyByValue } from '../utils/DOMUtils.js';
 import { quizData } from '../data.js';
+const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
 
-export const showCurrentQuestion = () => {
-  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+export const nextQuestionIndex = () => {
+  quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+}
 
-  const questionDOM = createQuestionElement(currentQuestion);
+const createQuestionDOM = () => {  
+  return createQuestionElement(currentQuestion);
+}
 
+const clearQuestionContainer = () => {
   const questionContainer = getDOMElement(QUESTION_CONTAINER_ID);
   clearDOMElement(questionContainer);
-  questionContainer.appendChild(questionDOM);
-};
+  questionContainer.appendChild(createQuestionDOM());
+}
+
+export const showCurrentQuestion = () => {
+  createQuestionDOM();
+  clearQuestionContainer();
+}
+
+const selectAnswer = (e) => {
+    currentQuestion.selected =  getKeyByValue(currentQuestion.answers, e.target.innerHTML)
+    console.log(currentQuestion)
+}
+
+document.addEventListener('click', selectAnswer) 
 
 export const handleNextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
