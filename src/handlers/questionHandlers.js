7927b4fcea320df818087fe612324c6e5ba030c7
AlexanderSaid <alexander.t.said@gmail.com>
2021-11-08 06:39:23 +0100

    First attempt to refactor the handlers

--- src/handlers/questionHandlers.js
+++ src/handlers/questionHandlers.js
@@ -5,18 +5,21 @@ import { createQuestionElement } from '../views/questionViews.js';
 import { clearDOMElement, getDOMElement } from '../utils/DOMUtils.js';
 import { quizData } from '../data.js';
 
-export const showCurrentQuestion = () => {
+export const nextQuestionIndex = () => {
+  quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+}
+const createQuestionDOM = () => {
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+  
+  return createQuestionElement(currentQuestion);
+}
 
-  const questionDOM = createQuestionElement(currentQuestion);
-
+const clearQuestionContainer = () => {
   const questionContainer = getDOMElement(QUESTION_CONTAINER_ID);
   clearDOMElement(questionContainer);
-  questionContainer.appendChild(questionDOM);
-};
-
-export const handleNextQuestion = () => {
-  quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
-
-  showCurrentQuestion();
-};
+  questionContainer.appendChild(createQuestionDOM());
+}
+export const showCurrentQuestion = () => {
+  createQuestionDOM();
+  clearQuestionContainer();
+}
