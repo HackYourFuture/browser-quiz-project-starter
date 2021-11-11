273d67987229a4ca56c96e59d8e0e7a049e043cb
SaidYein <saidyein@gmail.com>
2021-11-11 13:38:44 +0100

    going to next question without answer bug fixed

--- src/views/questionViews.js
+++ src/views/questionViews.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { NEXT_QUESTION_BUTTON_ID } from '../constants.js';
-import { nextQuestion, selectedAnswer } from '../listeners/questionListeners.js';
+import { selectedAnswer } from '../listeners/questionListeners.js';
 import { createDOMElement } from '../utils/DOMUtils.js';
 
 /**
@@ -10,7 +10,7 @@ import { createDOMElement } from '../utils/DOMUtils.js';
 export const createAnswerElement = (answerText) => {
   const answerElement = createDOMElement('li');
   answerElement.innerText = answerText;
-  answerElement.addEventListener('click', selectedAnswer);
+  answerElement.addEventListener('click', selectedAnswer)
   return answerElement;
 };
 
@@ -44,7 +44,6 @@ export const createNextQuestionButtonElement = () => {
   });
 
   buttonElement.innerText = 'Next question';
-  buttonElement.addEventListener('click', nextQuestion);
 
   return buttonElement;
 };
