0ade74ead82158f8c68bacea9fb557ff8cd78f89
Sina Boby <sinaboby91@yahoo.com>
2022-01-25 05:44:32 +0100

    chooseAnswer function defined & set as answersElement eventlistener

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -9,6 +9,7 @@ import { router } from '../router.js';
 
 export const initQuestionPage = (userInterface) => {
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+  console.log(currentQuestion);
 
   const questionElement = getQuestionElement(currentQuestion.text);
   userInterface.appendChild(questionElement);
@@ -17,7 +18,32 @@ export const initQuestionPage = (userInterface) => {
 
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
+    console.log(key);
+    answerElement.setAttribute('data-key', key);
+    console.log(answerElement.dataset);
     answersListElement.appendChild(answerElement);
+  }
+  console.log(answersListElement);
+  answersListElement.addEventListener('click', chooseAnswer);
+  function chooseAnswer(e) {
+    currentQuestion.selected = e.target.dataset.key;
+    console.log(e.target);
+    if (currentQuestion.selected !== currentQuestion.correct) {
+      console.log('false');
+      e.target.style.border = '2px solid red';
+    } else {
+      console.log('true');
+      e.target.style.border = '2px solid green';
+    }
+    console.dir(this);
+    const options = Array.from(this.children);
+    console.log(options);
+    options.forEach((option) => {
+      if (option.dataset.key !== currentQuestion.selected) {
+        option.style.border = '';
+      }
+      
+    });
   }
 
   document
