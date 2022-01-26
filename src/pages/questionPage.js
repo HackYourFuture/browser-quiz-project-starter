e57f5939090c82dcff5851927e17fdeca1784f57
Fedor Babikov <fed.babikov@protonmail.com>
2022-01-26 17:36:37 +0100

    deleted logs and commented code in src/pages/questionPage.js

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -9,36 +9,31 @@ import { quizData } from '../data.js';
 import { router } from '../router.js';
 
 export const initQuestionPage = (userInterface) => {
-  const currentQuestion = quizData.questions[quizData.currentQuestionIndex++]; //get CURRENT object
-  let alreadyChoosen = false;
+  const currentQuestion = quizData.questions[quizData.currentQuestionIndex++];
+  let alreadyChosen = false;
   const questionElement = getQuestionElement(
     currentQuestion.text,
     isLastQuestion()
-  ); //get Q-A div
+  );
   userInterface.appendChild(questionElement);
 
-  const answersListElement = document.getElementById(ANSWERS_LIST_ID); //get the answers UL
+  const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
-    //populate it with LI's
     const answerElement = createAnswerElement(key, answerText);
     answerElement.setAttribute('data-key', key);
     answersListElement.appendChild(answerElement);
-    //answerElement.addEventListener('click', chooseAnswer);
   }
   const options = Array.from(answersListElement.children);
   options.forEach( option => {
-    if (!alreadyChoosen){
+    if (!alreadyChosen){
       option.addEventListener('click', chooseAnswer);
     }
   })
-  //answersListElement.addEventListener('click', chooseAnswer);  //put EvListener on the UL
 
   function chooseAnswer(e) {
-    alreadyChoosen = true;
-    console.log(alreadyChoosen)
+    alreadyChosen = true;
     currentQuestion.selected = e.target.dataset.key;
-    //const options = Array.from(answersListElement.children);
     options.forEach((option) => {
       if (option.dataset.key !== currentQuestion.selected) {
         option.className = '';
@@ -59,11 +54,7 @@ export const initQuestionPage = (userInterface) => {
     
   }
   
-  
-   
-   
-
-  document //show the right button -> put EvListener on it -> on click choose the next step
+  document
     .getElementById(
       isLastQuestion() ? GET_RESULT_BUTTON_ID : NEXT_QUESTION_BUTTON_ID
     )
@@ -71,8 +62,6 @@ export const initQuestionPage = (userInterface) => {
 };
 
 const nextStep = () => {
-  //decide: where to go next - next Q or the RESULT page ?
-
   if (isLastQuestion()) {
     router('result');
   } else {
@@ -81,7 +70,6 @@ const nextStep = () => {
 };
 
 const isLastQuestion = () => {
-  //returns true/false - is the CURRENT Q the last or not?
   return quizData.currentQuestionIndex < quizData.questions.length
     ? false
     : true;
