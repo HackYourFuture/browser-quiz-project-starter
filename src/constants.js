4a3fa535e77946bb25f959488ec86fcb0fbf3625
Sina Boby <sinaboby91@yahoo.com>
2022-01-27 20:45:10 +0100

    constant result added

--- src/constants.js
+++ src/constants.js
@@ -13,4 +13,5 @@ export const NEXT_QUESTION_BUTTON_ID = 'next-question-button';
 export const START_QUIZ_BUTTON_ID = 'start-quiz-button';
 export const GET_RESULT_BUTTON_ID = 'get-result-button';
 export const TIMER_ELEMENT_ID = 'timer';
+export const REFERENCE_LIST_ID = 'reference-list';
 
