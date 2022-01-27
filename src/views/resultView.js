7193f1bf6747d6d79c73ca5bdc80bc576a85a941
Sina Boby <sinaboby91@yahoo.com>
2022-01-27 20:46:37 +0100

    result page feature added

--- src/views/resultView.js
+++ src/views/resultView.js
@@ -1,9 +1,18 @@
 'use strict';
 
 
+import { REFERENCE_LIST_ID } from '../constants.js';
+import { quizData } from '../data.js';
 
 export const createResultElement = () => {
     const element = document.createElement('div');
-    element.innerHTML = String.raw`<h1>This is the result page</h1>`;
+    element.innerHTML = String.raw`
+        <h1>Congratulations!</h1>
+        <p>You have finished the quiz about JavaScript.</p>
+        <p>Correct answers: ${quizData.correctSum}</p>
+        <p>Wrong answers: ${quizData.wrongSum}</p>
+        <p>For more about JavaScript, visit:</p>
+        <ol id="${REFERENCE_LIST_ID}"></ol>
+    `;
     return element;
 }
