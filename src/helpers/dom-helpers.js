76eac31c052d18592d9a4fa86981e2e4662fa563
Jim Cramer <remarcmij@gmail.com>
2022-03-02 15:07:20 +0100

    Refactor fn  defs to arrow fn for consistency

--- src/helpers/dom-helpers.js
+++ src/helpers/dom-helpers.js
@@ -1,10 +1,10 @@
-export function createElement(tagName = 'div', options = {}) {
+export const createElement = (tagName = 'div', options = {}) => {
   const elem = document.createElement(tagName);
 
   // `text` is not a regular attribute but must be set via `.textContent`
   const { text, ...rest } = options;
 
-  // set proper attributes
+  // set regular attributes
   for (const [name, value] of Object.entries(rest)) {
     elem.setAttribute(name, value);
   }
@@ -15,10 +15,10 @@ export function createElement(tagName = 'div', options = {}) {
   }
 
   return elem;
-}
+};
 
-export function clearElement(elem) {
+export const clearElement = (elem) => {
   while (elem.firstChild) {
     elem.removeChild(elem.firstChild);
   }
-}
+};
