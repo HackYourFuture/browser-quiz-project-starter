842b7fec48e55a052c91039bc89021fd50c5e785
Radhi Hudijan <radhif2009@gmail.com>
2022-03-06 21:29:45 +0100

    Basic Questions logic and Css styling

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -9,23 +9,59 @@ import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
 
+let acceptingAnswers = false;
+
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
 
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
 
+  //Creating the question HTML
   const questionElement = createQuestionElement(currentQuestion.text);
 
   userInterface.appendChild(questionElement);
 
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
+  //Creating answer list and add Data key attribute
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
+    answerElement.setAttribute('data-key', key);
     answersListElement.appendChild(answerElement);
   }
 
+  acceptingAnswers = true;
+  // add click event to answer choices and select the correct one
+  for (const option of answersListElement.children) {
+    option.addEventListener('click', chooseAnswer);
+  }
+
+  function chooseAnswer() {
+    // check if the question already loaded
+    if (!acceptingAnswers) return;
+    acceptingAnswers = false;
+
+    currentQuestion.selected = this.dataset.key;
+
+    // set the correct and incorrect class to the answer selection
+    const classToApply =
+      currentQuestion.selected === currentQuestion.correct
+        ? 'correct'
+        : 'incorrect';
+
+    if (currentQuestion.selected == currentQuestion.correct) {
+      this.classList.add(classToApply);
+    } else {
+      this.classList.add(classToApply);
+      const correctAnswer = document.querySelector(
+        `li[data-key="${currentQuestion.correct}"]`
+      );
+      correctAnswer.classList.add('correct-answer');
+    }
+  }
+
+  // Next question button
   document
     .getElementById(NEXT_QUESTION_BUTTON_ID)
     .addEventListener('click', nextQuestion);
--- src/views/answerView.js
+++ src/views/answerView.js
@@ -6,8 +6,10 @@
  */
 export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
+  element.classList.add('choice-container');
   element.innerHTML = String.raw`
-    ${key}: ${answerText};
+  <p class="choice-prefix">${key}</p>
+  <p class="choice-text">${answerText}</p>
   `;
   return element;
 };
