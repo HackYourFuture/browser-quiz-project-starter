d0c53ae4fbb298ad117a36bf9c0cd1957ea05183
mohamed belal <mhbbelal22@gmail.com>
2022-08-30 07:40:26 +0200

    adding correct answers variable in dataObject and creating final summary page

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 
 import { quizData } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
+import { finalSummaryPage } from './pages/finalSummaryPage.js';
 
 const loadApp = () => {
   quizData.currentQuestionIndex = 0;
-
-  initWelcomePage();
+  // console.log(quizData)
+  // initWelcomePage();
+  finalSummaryPage()
 };
 
 window.addEventListener('load', loadApp);
