5887c0dfc9ff1be240a157202ced4218c5abde72
Elif Nur Kalkan <elifnkalkan@gmail.com>
2022-08-30 16:23:50 +0200

    the error fixed at line 58

--- src/data.js
+++ src/data.js
@@ -54,7 +54,7 @@ export const quizData = {
         {
           text: 'F1Mix',
           href:
-            'https://f1mix.com/results/2015-formula-1-world-championship/,
+            'https://f1mix.com/results/2015-formula-1-world-championship/',
         },
       ],
     },
