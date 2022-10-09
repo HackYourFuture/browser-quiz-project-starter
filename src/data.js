a21bef64fb4175c2c878911c40398b32ee0114c5
anna <anutka2610@gmail.com>
2022-10-09 16:47:40 +0200

    test

--- src/data.js
+++ src/data.js
@@ -1,15 +1,7 @@
 'use strict';
 
-/* Program Data
+/* just test
 
-  in this file you can declare variables to store important data for your program
-  the data can only be primitives, objects or arrays
-  do not store dom elements in these variables!!!!
-
-  these variables will be imported by your handlers when necessary
-    not by your logic
-    not by your listeners
-*/
 
 export const quizData = {
   currentQuestionIndex: 0,
