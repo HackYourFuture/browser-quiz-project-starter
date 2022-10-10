b84d114e77b9f32b079bea6c96a2ed1b22f2d34c
mysr3809 <myasar0938@gmail.com>
2022-10-10 19:57:17 +0200

    tasks done

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -4,8 +4,13 @@
  * Create an Answer element
  * @returns {Element}
  */
+
 export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
+  element.onclick = (e) => {
+    const selectedOption = e.target.innerText[0]
+    console.log(selectedOption)
+  };
   element.innerHTML = String.raw`
     ${key}: ${answerText};
   `;
