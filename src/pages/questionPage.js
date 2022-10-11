b35f4257790286411fad6ed871d40a2e835f904f
Yasserhub <yasser.a.murshed@gmail.com>
2022-10-11 18:48:44 +0200

    Disable other answers once one is selected

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -1,51 +1,92 @@
+'use strict';
+
 import {
   ANSWERS_LIST_ID,
   NEXT_QUESTION_BUTTON_ID,
   USER_INTERFACE_ID,
 } from '../constants.js';
 import { createQuestionElement } from '../views/questionView.js';
+import { createScoreElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
-import { createProgressElement } from '../views/progressView.js';
-import { selectAnswerVariant } from '../views/selectedAnswerView.js';
-
-let rightAnswer;
 
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
 
+  const socreOfCorrectAnswers = createScoreElement("Score");
+  userInterface.appendChild(socreOfCorrectAnswers);
+
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
 
   const questionElement = createQuestionElement(currentQuestion.text);
-  userInterface.appendChild(questionElement);
 
-  const progressBlock = createProgressElement();
-  userInterface.appendChild(progressBlock);
+  userInterface.appendChild(questionElement);
 
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
     answersListElement.appendChild(answerElement);
-
-    answerElement.addEventListener('click', selectedAnswer);
-
-    if (key === currentQuestion.correct) {
-      rightAnswer = answerElement;
-    }
   }
+
+
+
+  const buttonDeActivation= document.querySelectorAll('.test');
+
+             buttonDeActivation.forEach((buttonId) => {
+              buttonId.addEventListener('click',function() {
+                const y = buttonId.id; 
+                 if ( currentQuestion.correct === y){
+                  buttonId.style.background = "green";
+                  quizData.finalScore = quizData.finalScore + 1;
+                 
+                  myDisplay(quizData.finalScore);                 
+
+                  myDisplay(`score ${quizData.finalScore}`);
+                  answerButtonDisable();
+
+                 } 
+                 else {
+                  buttonId.style.background="red";
+                  console.log(quizData.finalScore);
+
+                  myDisplay(quizData.finalScore);
+                  
+                  answerButtonDisable();
+                 } 
+              });  // end addEventListener
+                
+              });  // end forEach
+            
+      
+      
 
   document
     .getElementById(NEXT_QUESTION_BUTTON_ID)
     .addEventListener('click', nextQuestion);
 };
 
-const nextQuestion = () => {
+  
+
+  const answerButtonDisable = () => {
+    const buttonDeActivation= document.querySelectorAll('.test');
+    buttonDeActivation.forEach((buttonDeactive) => {
+     buttonDeactive.disabled=true;
+    });
+};
+ 
+
+function myDisplay(score) {
+  
+  document.getElementById("updatedScore").innerHTML += score;
+}
+
+  const nextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+   
+   
   initQuestionPage();
 };
 
-export const selectedAnswer = (e) => {
-  selectAnswerVariant(e.target, rightAnswer);
-};
+
--- src/views/answerView.js
+++ src/views/answerView.js
@@ -1,11 +1,18 @@
+'use strict';
+
 /**
  * Create an Answer element
  * @returns {Element}
  */
 export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
+  // element.innerHTML = String.raw`
+  //   ${key}: ${answerText};
+  // `;
   element.innerHTML = String.raw`
-    ${key}: ${answerText};
+    
+    <button class="test" id=${key}>${key}: ${answerText}</button>
   `;
+  
   return element;
 };
