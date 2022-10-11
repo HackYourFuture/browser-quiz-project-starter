3e68d1c3956eb89e756f7d465fdb047a21fcdbbd
Alqayadhi <mo.alqayadhi@gmail.com>
2022-10-11 13:47:54 +0200

    finalpage-finalview

---
+++ src/views/finalView.js
@@ -0,0 +1,10 @@
+import {quizData} from '../data.js';
+
+export const createFinalSummaryElement = () => {
+    const { finalScore, questions } = quizData;
+  
+    const element = document.createElement('div');
+    element.innerHTML = `
+      <h1 class='final'>Congratulations ! You answered ${finalScore} correct from  ${questions.length} questions!</h1>`;
+    return element;
+  };
