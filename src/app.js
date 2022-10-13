73f701200cfd78d162686b1348f87ff77f6cf1ba
mysr3809 <myasar0938@gmail.com>
2022-10-13 14:49:09 +0200

    tasks done(answer localstorage-scoreCount)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { quizData } from './data.js';
-import { initQuestionPage } from './pages/questionPage.js';
+import { initQuestionPage, } from './pages/questionPage.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 
 const loadApp = () => {
@@ -9,10 +9,26 @@ const loadApp = () => {
   quizData.currentQuestionIndex = storedIndex ? parseInt(storedIndex) : 0;
   const storedFinalScore = localStorage.getItem('finalScore');
   quizData.finalScore = storedFinalScore ? parseInt(storedFinalScore) : 0;
-  const answers = localStorage.getItem('selectedAnswer');
 
   if (storedIndex) {
     initQuestionPage();
+    const selectedAnswer = localStorage.getItem('selectedAnswer');
+    const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+    const correctAnswerElement = document.getElementById(currentQuestion.correct);
+    const selectedAnswerElement = document.getElementById(selectedAnswer);
+    const liTags = document.getElementsByTagName('li');
+    if (selectedAnswer === currentQuestion.correct) {
+      correctAnswerElement.style.backgroundColor = 'green';
+      correctAnswerElement.style.color = 'white';
+    } else {
+      correctAnswerElement.style.backgroundColor = 'green';
+      correctAnswerElement.style.color = 'white';
+      selectedAnswerElement.style.backgroundColor = 'red';
+      selectedAnswerElement.style.color = 'white';
+    }
+    for (let liTag of liTags) {
+      liTag.style.pointerEvents = 'none'
+    }
   } else {
     initWelcomePage();
   }
--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -9,54 +9,60 @@ import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
 import { initFinishPage } from './finishPage.js';
+import { createScoreElement, updateScore } from '../views/scoreView.js'
 
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
 
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
-  console.log(currentQuestion);
-  localStorage.setItem('currentQuestionIndex', quizData.currentQuestionIndex)
+  localStorage.setItem('currentQuestionIndex', quizData.currentQuestionIndex);
 
   const questionElement = createQuestionElement(currentQuestion.text);
   userInterface.appendChild(questionElement);
-  // const answer = localStorage.getItem('selectedAnswer');
-  // console.log(answer);
-  // if (answer[quizData.currentQuestionIndex]) {
-  //   currentQuestion.selected = answer[quizData.currentQuestionIndex];
-  //   // TODO show the button later
-  // }
+  const scoreElement = createScoreElement();
+  userInterface.insertBefore(scoreElement, userInterface.firstChild);
+
   const correctAnswer = (event) => {
     if (currentQuestion.selected != null) {
       return;
     }
 
     const selectedAnswer = event.target.innerText[0];
+    localStorage.setItem('selectedAnswer', selectedAnswer);
+
     console.log(selectedAnswer);
     console.log('test');
     const correctAnswer = currentQuestion.correct;
-
-    if (selectedAnswer === correctAnswer) {
-
-
-    } else {
-      console.log(false);
-    }
   }
 
-  const showCorrectAnswer = () => {
+  const showCorrectAnswer = (event) => {
     if (currentQuestion.selected != null) {
       return;
     }
 
     const correctAnswerElement = document.getElementById(currentQuestion.correct);
-    setInterval(() => {
-      correctAnswerElement.style.backgroundColor = 'green';
-      correctAnswerElement.style.color = 'white';
+    const selectedAnswerElement = document.getElementById(event.target.innerText[0]);
+    const liTags = document.getElementsByTagName('li');
+    setTimeout(() => {
+      if (event.target.innerText[0] === currentQuestion.correct) {
+        correctAnswerElement.style.backgroundColor = 'green';
+        correctAnswerElement.style.color = 'white';
+        quizData.finalScore++;
+        updateScore();
+      } else {
+        correctAnswerElement.style.backgroundColor = 'green';
+        correctAnswerElement.style.color = 'white';
+        selectedAnswerElement.style.backgroundColor = 'red';
+        selectedAnswerElement.style.color = 'white';
+      }
+      for (let liTag of liTags) {
+        liTag.style.pointerEvents = 'none'
+      }
+      localStorage.setItem('finalScore', quizData.finalScore);
     }, 1000);
 
   }
-
 
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
@@ -66,9 +72,6 @@ export const initQuestionPage = () => {
     answerElement.addEventListener('click', correctAnswer);
     answerElement.addEventListener('click', showCorrectAnswer);
   }
-
-
-
   document
     .getElementById(NEXT_QUESTION_BUTTON_ID)
     .addEventListener('click', nextQuestion);
