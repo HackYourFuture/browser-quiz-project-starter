416bcf4db4b3e2a41bd131325cc0879df091e990
Abdullah Omer ALIMOGLU <aoalimoglu@gmail.com>
2023-01-30 12:12:21 +0100

    I added a quiz rules page, review and fixed the quizRulesView.js and also add a quit quiz button id to the constants.

---
+++ src/pages/quizRulesPage.js
@@ -0,0 +1,31 @@
+import {
+  USER_INTERFACE_ID,
+  START_QUIZ_BUTTON_ID,
+  QUIT_QUIZ_BUTTON_ID,
+} from '../constants.js';
+import { createQuizRulesElement } from '../views/welcomeView.js';
+import { initQuestionPage } from './questionPage.js';
+
+export const initQuizRulesPage = () => {
+  const userInterface = document.getElementById(USER_INTERFACE_ID);
+  userInterface.innerHTML = '';
+
+  const rulesElement = createQuizRulesElement();
+  userInterface.appendChild(rulesElement);
+
+  document
+    .getElementById(START_QUIZ_BUTTON_ID)
+    .addEventListener('click', startQuiz);
+
+  document
+    .getElementById(QUIT_QUIZ_BUTTON_ID)
+    .addEventListener('click', quitQuiz);
+};
+
+const quitQuiz = () => {
+  initWelcomePage();
+};
+
+const startQuiz = () => {
+  initQuestionPage();
+};
