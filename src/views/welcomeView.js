fed6c2fa0246da025333bf7d4155c8e9e1b8d79c
NojdarTaho <97285902+NojdarTaho@users.noreply.github.com>
2023-01-30 15:35:08 +0100

    Made a simple welcome page

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -6,9 +6,14 @@ import { START_QUIZ_BUTTON_ID } from '../constants.js';
  */
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
+  const image = document.createElement('img');
+  element.className = 'container';
   element.innerHTML = String.raw`
+  <img src="./public/wallhaven-od6z1p.jpeg" alt="">
+
     <h1>Welcome</h1>
-    <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
+    <button id="${START_QUIZ_BUTTON_ID}">start quiz!</button>
+
   `;
   return element;
 };
