655bddd46e1cc97f3eae8580f11493f834e95c4e
Ozgur Ozhan <ozgur2096@gmail.com>
2023-01-30 23:36:34 +0100

    it has to be clicked on the answers only once

--- src/views/checker.js
+++ src/views/checker.js
@@ -7,28 +7,33 @@ export const checker = function (){
   const correctAnswer = quizData.questions[quizData.currentQuestionIndex].correct; 
 
   const clickChecker = function () {
+    
     answers.forEach(answer => answer.addEventListener('click', (e)=> {
-      const clickedAnswer = e.target.dataset.key;
-      quizData.questions[quizData.currentQuestionIndex].selected = clickedAnswer
+      if (!quizData.questions[quizData.currentQuestionIndex].selected) {
+        const clickedAnswer = e.target.dataset.key;
+        quizData.questions[quizData.currentQuestionIndex].selected = clickedAnswer
       
-      if (correctAnswer  === clickedAnswer) {
-        document.body.style.background = 'green'
-      }
-      else document.body.style.background = 'red'
-      playSound();
-    }));    
+        if (correctAnswer  === clickedAnswer) {
+          document.body.style.background = 'green'
+        }
+        else document.body.style.background = 'red'
+        playSound();
+      }  
+    }));  
   }
   
   const keydownChecker = function (){
     window.addEventListener('keydown', (e) => {
-      if (e.key === 'a' || e.key === 'b' || e.key === 'c' || e.key === 'd') {
-        quizData.questions[quizData.currentQuestionIndex].selected = e.key;
-        if(e.key === correctAnswer) {
-          document.body.style.background = 'green'
+      if (!quizData.questions[quizData.currentQuestionIndex].selected) {
+        if (e.key === 'a' || e.key === 'b' || e.key === 'c' || e.key === 'd') {
+          quizData.questions[quizData.currentQuestionIndex].selected = e.key;
+          if(e.key === correctAnswer) {
+            document.body.style.background = 'green'
+          }
+          else {document.body.style.background = 'red'}
+          playSound();
         }
-        else {document.body.style.background = 'red'}
-        playSound();
-      }
+      }  
     })
   }
   clickChecker();      
