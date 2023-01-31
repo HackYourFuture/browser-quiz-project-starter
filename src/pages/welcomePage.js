d7b25f501331fa5130db4a7b82d447228784dbae
NojdarTaho <97285902+NojdarTaho@users.noreply.github.com>
2023-01-31 14:38:58 +0100

    fixed some things

--- src/pages/welcomePage.js
+++ src/pages/welcomePage.js
@@ -1,19 +1,19 @@
-import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID } from '../constants.js';
-import { createWelcomeElement } from '../views/welcomeView.js';
-import { initQuestionPage } from './questionPage.js';
+import { USER_INTERFACE_ID, QUIZ_BUTTON_ID } from '../constants.js';
+import { createQuizRulesElement } from '../views/welcomeView.js';
+import { initQuizRulesPage } from '../pages/quizRulesPage.js'
 
 export const initWelcomePage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
 
-  const welcomeElement = createWelcomeElement();
+  const welcomeElement = createQuizRulesElement();
   userInterface.appendChild(welcomeElement);
 
   document
-    .getElementById(START_QUIZ_BUTTON_ID)
-    .addEventListener('click', startQuiz);
+    .getElementById(QUIZ_BUTTON_ID)
+    .addEventListener('click', rules);
 };
 
-const startQuiz = () => {
-  initQuestionPage();
+const rules = () => {
+  initQuizRulesPage();
 };
--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -1,18 +1,18 @@
-import { START_QUIZ_BUTTON_ID } from '../constants.js';
+import { QUIZ_BUTTON_ID } from '../constants.js';
 
 /**
  * Create the welcome screen
  * @returns {Element}
  */
-export const createWelcomeElement = () => {
+export const createQuizRulesElement = () => {
   const element = document.createElement('div');
-  const image = document.createElement('img');
+  
   element.className = 'container';
   element.innerHTML = String.raw`
   <img src="./public/wallhaven-od6z1p.jpeg" alt="">
 
     <h1>Welcome</h1>
-    <button id="${START_QUIZ_BUTTON_ID}">start quiz!</button>
+    <button id="${ QUIZ_BUTTON_ID }">Rules</button>
 
   `;
   return element;
