4c8705983a3d21bb1a99435e8d35dc07c894816d
sima-milli <sima.milli@gmail.com>
2023-02-01 11:42:59 +0100

    revert app.js change

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ import { initQuestionPage } from './pages/questionPage.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 
 const loadApp = () => {
-  // testing questions page for now, we need to change this back to welcome page initiator once it's ready
-  initQuestionPage();
+  initWelcomePage();
 };
 
 window.addEventListener('load', loadApp);
