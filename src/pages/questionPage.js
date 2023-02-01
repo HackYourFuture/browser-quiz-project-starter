e097be4528bf35e8447f05ee505eea11a73c660a
Daniil <daniil.kravchenko.92@yandex.ru>
2023-02-01 18:27:49 +0100

    result button, logic not implemented

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -2,7 +2,6 @@ import { quizData } from '../data.js';
 import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { USER_INTERFACE_ID } from '../constants.js';
-import { initResultsPage } from '../pages/resultsPage.js';
 
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
@@ -31,12 +30,13 @@ const nextQuestion = () => {
 
   initQuestionPage();
   // logic to remove "next question" and to add "view results button"
-  if(quizData.currentQuestionIndex === 9){
+  // addEventListener doesnt work
+  if (quizData.currentQuestionIndex === 9) {
     const resultButton = document.getElementById(NEXT_QUESTION_BUTTON_ID);
-    resultButton.textContent = "View results";
-    resultButton.id = 'show-results-button'
+    resultButton.textContent = 'View results';
+    resultButton.id = 'show-results-button';
     document
-    .getElementById('show-results-button')
-    .addEventListener('click', initResultsPage);
+      .getElementById('show-results-button')
+      .addEventListener('click', initResultsPage);
   }
 };
