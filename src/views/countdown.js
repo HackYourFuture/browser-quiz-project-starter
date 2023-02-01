fc8f6e9fc5bbf2864b6c1d3ec2678ffd07b1f09f
Jalal Habeeb <jalalhabeeb@gmail.com>
2023-02-01 18:07:10 +0100

    creat countdown p element in vanilla JS

--- src/views/countdown.js
+++ src/views/countdown.js
@@ -1,20 +1,24 @@
 import { COUNT_DOWN } from '../constants.js';
 import { nextQuestion } from '../pages/questionPage.js';
+
+const paragraphElement = document.createElement('p');
+paragraphElement.setAttribute('id', COUNT_DOWN);
+document.body.appendChild(paragraphElement);
+
 /*countdown start */
 let time = 60;
 const countdownEl = document.getElementById(COUNT_DOWN);
 export const startCountdown = () => {
-  
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    minutes = minutes < 10 ? `0${minutes}` : minutes;
-    seconds = seconds < 10 ? `0${seconds}` : seconds;
-    countdownEl.innerHTML = `${minutes} : ${seconds}`;
-    time--;
-    
-    if (time === 0) {
-        nextQuestion();
-        time = 60;
-    }
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+  countdownEl.innerHTML = `${minutes} : ${seconds}`;
+  time--;
+
+  if (time === 0) {
+    nextQuestion();
+    time = 60;
+  }
 };
 /*countdown end */
