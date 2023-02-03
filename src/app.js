ab6b1142b3a118482b5719f4501ccd40119dab38
Sally-samaan <samaan.sally@gmail.com>
2023-02-03 19:16:43 +0100

    adjusted the fvicone and title in the new branch

--- src/app.js
+++ src/app.js
@@ -3,8 +3,18 @@ import { initWelcomePage } from './pages/welcomePage.js';
 
 const loadApp = () => {
   quizData.currentQuestionIndex = 0;
+  document.title = 'Singularity';
 
   initWelcomePage();
 };
+
+function setFavicons(favImg){
+  let headTitle = document.querySelector('head');
+  let setFavicon = document.createElement('link');
+  setFavicon.setAttribute('rel','shortcut icon');
+  setFavicon.setAttribute('href',favImg);
+  headTitle.appendChild(setFavicon);
+}
+setFavicons('/public/images/3285297-andromeda-astronomy-cosmos-galaxy-space-spiral-universe_106791.png');
 
 window.addEventListener('load', loadApp);
