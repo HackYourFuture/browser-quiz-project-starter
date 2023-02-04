caa5cfad90e13b464dcd8a6d8562babcbde74385
Ahmet Cigil <ahmetcgln@gmail.com>
2023-02-05 00:52:59 +0100

    commiting countdown and shadow to css h1

--- src/pages/welcomePage.js
+++ src/pages/welcomePage.js
@@ -1,7 +1,7 @@
 import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID } from '../constants.js';
 import { createWelcomeElement } from '../views/welcomeView.js';
 import { initQuestionPage } from './questionPage.js';
-import { startCountdown } from '../views/countdown.js';
+import { startCountdown } from './countdown.js';
 
 export const initWelcomePage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
@@ -20,18 +20,18 @@ export const initWelcomePage = () => {
     });
 };
 
-
-
-function setFavicons(favImg){
+function setFavicons(favImg) {
   const headTitle = document.querySelector('head');
   const setFavicon = document.createElement('link');
-  setFavicon.setAttribute('rel','shortcut icon');
-  setFavicon.setAttribute('href',favImg);
+  setFavicon.setAttribute('rel', 'shortcut icon');
+  setFavicon.setAttribute('href', favImg);
   headTitle.appendChild(setFavicon);
 }
-setFavicons('/public/images/3285297-andromeda-astronomy-cosmos-galaxy-space-spiral-universe_106791.png');
+setFavicons(
+  '/public/images/3285297-andromeda-astronomy-cosmos-galaxy-space-spiral-universe_106791.png'
+);
 
-document.title = "Singularity";
+document.title = 'Singularity';
 
 const startQuiz = () => {
   initQuestionPage();
