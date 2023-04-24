b5c844e237bd3d5e72c5b1144384188597cceb16
EuvhenRight <ugnivenko.ea@gmail.com>
2023-04-24 13:12:24 +0200

    create disable button and countQestion and progressivebar

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -6,6 +6,8 @@ import {
 import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
+import { changeProgress } from '../views/progressBar.js';
+
 
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
@@ -13,7 +15,7 @@ export const initQuestionPage = () => {
 
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
 
-  const questionElement = createQuestionElement(currentQuestion.text);
+  const questionElement = createQuestionElement(currentQuestion.text, quizData.currentQuestionIndex + 1);
 
   userInterface.appendChild(questionElement);
 
@@ -22,11 +24,24 @@ export const initQuestionPage = () => {
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
     answersListElement.appendChild(answerElement);
+
   }
 
+// this code disabled and active button
   document
-    .getElementById(NEXT_QUESTION_BUTTON_ID)
-    .addEventListener('click', nextQuestion);
+  .getElementById(NEXT_QUESTION_BUTTON_ID)
+  .setAttribute('disabled', true)
+
+  // Add event listener to the button
+  document
+  .getElementById(NEXT_QUESTION_BUTTON_ID)
+  .removeAttribute('disabled')
+
+  document
+  .getElementById(NEXT_QUESTION_BUTTON_ID)
+  .addEventListener('click', nextQuestion);
+
+  const currentBar = changeProgress((quizData.currentQuestionIndex * 10)+10); // Progressbar line
 };
 
 const nextQuestion = () => {
@@ -34,3 +49,7 @@ const nextQuestion = () => {
 
   initQuestionPage();
 };
+
+
+
+
