3666d0fbb3a8bffa7b5399432881e1c5112f57e8
Anastasia Tikhonova <honu347@gmail.com>
2023-04-24 12:49:10 +0200

    added select answers, show correct answer, select only one

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -22,6 +22,8 @@ export const initQuestionPage = () => {
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
     answersListElement.appendChild(answerElement);
+    answerElement.setAttribute('data-key', key);
+    answerElement.addEventListener('click', selectAnswer)
   }
 
   document
@@ -29,8 +31,49 @@ export const initQuestionPage = () => {
     .addEventListener('click', nextQuestion);
 };
 
+ //Code repeat 
+const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+const correctAnswerKey = currentQuestion.correct;
+
+// Flag for not selecting two items
+quizData.answerSelected = false;
+
+const selectAnswer = (event) => {
+ 
+  if (quizData.answerSelected) {
+    return;
+  }
+  
+    const selectedListItem = event.target;
+    const userAnswer = selectedListItem.dataset.key;
+
+    quizData.answerSelected = true;
+
+    if (userAnswer == currentQuestion.correct) {
+     selectedListItem.classList.add('yes')
+    } else {
+     selectedListItem.classList.add('no')
+     showCorrectAnswer()
+    }
+};
+
+const showCorrectAnswer = () => {
+  // Code repeat
+  const answersListElement = document.getElementById(ANSWERS_LIST_ID);
+  const answerElements = answersListElement.querySelectorAll('li');
+
+  answerElements.forEach((answerElement) => {
+    const answerKey = answerElement.dataset.key;
+
+    if (answerKey == correctAnswerKey) {
+      answerElement.classList.add('yes');
+    }
+  });
+};
+
 const nextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+  quizData.answerSelected = false;
 
   initQuestionPage();
 };
