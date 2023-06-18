3be9d4dd0b861d2d2172bf0602a207abef6f05d5
Enes Tasdemir <enstsdmr2135@gmail.com>
2023-06-18 23:52:57 +0200

    completed questions

--- src/data.js
+++ src/data.js
@@ -9,52 +9,190 @@
     not by your listeners
 */
 
+
 export const quizData = {
   currentQuestionIndex: 0,
   // the questions in the quiz
   questions: [
     {
-      text: 'What are the different ways to declare a JS variable?',
+      text: 'What is the capital city of France?',
       answers: {
-        a: 'constant, let, variable',
-        b: 'var, const, let, function',
-        c: 'var, let, const',
+        a: 'Paris',
+        b: 'Rome',
+        c: 'Madrid',
+        d: 'London',
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'paris.wikipedia',
+          href: 'https://www.worlddata.info/capital-cities.php',
+        },
+        
+      ],
+    },
+    {
+      text: 'What is the capital city of Australia?',
+      answers: {
+        a: 'Sydney',
+        b: 'Melbourne',
+        c: 'Canberra',
+        d: 'Perth',
       },
       correct: 'c',
       selected: null,
       links: [
         {
-          text: 'javascript.info',
-          href: 'https://javascript.info/variables',
+          text: 'canberra.wikipedia',
+          href: 'https://www.worlddata.info/capital-cities.php',
         },
-        {
-          text: 'Tyler McGinnis',
-          href: 'https://ui.dev/var-let-const/',
-        },
+        
       ],
     },
     {
-      text: 'What does `typeof` do?',
+      text: 'What is the capital city of Japan?',
       answers: {
-        a: 'changes the type of a primitive value',
-        b: 'returns a string describing the type of a value',
-        c: 'determines if a value is primitive',
-        d: 'can tell the difference between arrays and objects',
+        a: 'Bangkok',
+        b: 'Beijing',
+        c: 'Seoul',
+        d: 'Tokyo',
+      },
+      correct: 'd',
+      selected: null,
+      links: [
+        {
+          text: 'tokyo.wikipedia',
+          href: 'https://www.worlddata.info/capital-cities.php',
+        },
+        
+      ],
+    },
+    {
+      text: 'What is the capital city of Brazil?',
+      answers: {
+        a: 'Brasília',
+        b: 'São Paulo',
+        c: 'Rio de Janeiro',
+        d: 'Buenos Aires',
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'brasília.wikipedia',
+          href: 'https://www.worlddata.info/capital-cities.php',
+        },
+        
+      ],
+    },
+    {
+      text: 'What is the capital city of Canada?',
+      answers: {
+        a: 'Toronto',
+        b: 'Montreal',
+        c: 'Ottawa',
+        d: 'Vancouver',
+      },
+      correct: 'c',
+      selected: null,
+      links: [
+        {
+          text: 'ottawa.wikipedia',
+          href: 'https://www.worlddata.info/capital-cities.php',
+        },
+        
+      ],
+    },
+    {
+      text: 'What is the capital city of Indonesia?',
+      answers: {
+        a: 'Jakarta',
+        b: 'Surabaya',
+        c: 'Bandung',
+        d: 'Bangkok',
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'jakarta.wikipedia',
+          href: 'https://www.worlddata.info/capital-cities.php',
+        },
+        
+      ],
+    },
+    {
+      text: 'What is the capital city of Nepal?',
+      answers: {
+        a: 'Taipei',
+        b: 'Karachi',
+        c: 'Kathmandu',
+        d: 'Dhaka',
+      },
+      correct: 'c',
+      selected: null,
+      links: [
+        {
+          text: 'kathmandu.wikipedia',
+          href: 'https://www.worlddata.info/capital-cities.php',
+        },
+        
+      ],
+    },
+    {
+      text: 'What is the capital city of Russia?',
+      answers: {
+        a: 'St. Petersburg',
+        b: 'Minsk',
+        c: 'Vilnius',
+        d: 'Moscow',
+      },
+      correct: 'd',
+      selected: null,
+      links: [
+        {
+          text: 'moscow.wikipedia',
+          href: 'https://www.worlddata.info/capital-cities.php',
+        },
+        
+      ],
+    },
+    {
+      text: 'What is the capital city of Mexico?',
+      answers: {
+        a: 'Guadalajara',
+        b: 'Monterrey',
+        c: 'Mexico City',
+        d: 'Cancun',
+      },
+      correct: 'c',
+      selected: null,
+      links: [
+        {
+          text: 'mexico_city.wikipedia',
+          href: 'https://www.worlddata.info/capital-cities.php',
+        },
+        
+      ],
+    },
+    {
+      text: 'What is the capital city of Morocco?',
+      answers: {
+        a: 'Marrakesh',
+        b: 'Rabat',
+        c: 'Casablanca',
+        d: 'Tangier',
       },
       correct: 'b',
       selected: null,
       links: [
         {
-          text: 'javascript.info',
-          href: 'https://javascript.info/types#type-typeof',
+          text: 'rabat.wikipedia',
+          href: 'https://www.worlddata.info/capital-cities.php',
         },
-        {
-          text: 'MDN',
-          href:
-            'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
-        },
+        
       ],
     },
-    // Add more questions here
-  ],
+  ]
 };
