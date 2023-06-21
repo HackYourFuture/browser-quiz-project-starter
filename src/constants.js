1bd87a7e79ae70685cfc88fab3e7c0001b6d5629
Hadelmur <murshed.hadeel@gmail.com>
2023-06-21 13:36:28 +0200

    adding a title with favicon, select answer function, score styling and functionality

--- src/constants.js
+++ src/constants.js
@@ -9,3 +9,8 @@ export const USER_INTERFACE_ID = 'user-interface';
 export const START_QUIZ_BUTTON_ID = 'start-quiz-button';
 export const ANSWERS_LIST_ID = 'answers-list';
 export const NEXT_QUESTION_BUTTON_ID = 'next-question-button';
+
+export const ANSWER_BUTTONS = "answer-buttons";
+export const SCORE_ELEMENT = "score";
+export const SCORE_BUTTON = "score-btn";
+export const SCORE_MESSAGE = "score-message";
--- src/views/answerView.js
+++ src/views/answerView.js
@@ -2,6 +2,7 @@
  * Create an Answer element
  * @returns {Element}
  */
+
 export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
   element.innerHTML = String.raw`
@@ -9,3 +10,43 @@ export const createAnswerElement = (key, answerText) => {
   `;
   return element;
 };
+
+import { ANSWER_BUTTONS } from '../constants.js';
+import { SCORE_ELEMENT } from '../constants.js';
+import { SCORE_BUTTON } from '../constants.js';
+import { SCORE_MESSAGE } from '../constants.js';
+
+let score = 0
+
+ANSWER_BUTTONS.forEach(button => {
+    button.addEventListener("click", checkAnswer)
+})
+
+SCORE_BUTTON.addEventListener("click", showScoreMessage)
+
+function checkAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.classList.contains("correct")
+
+    if(isCorrect){
+        selectedBtn.innerHTML = "You got a point"
+        selectedBtn.classList.add("disabled")
+        score++
+        updateScore()
+    } else {
+        selectedBtn.innerHTML = "Sorry, no point"
+        selectedBtn.classList.add("disabled")
+    }
+}
+
+function showScoreMessage(){
+  SCORE_MESSAGE.innerHTML = `Your score: ${score} points`
+}
+
+function updateScore(){
+  let score = parseInt(SCORE_ELEMENT.textContent);
+  let newScore = score + 1
+  SCORE_ELEMENT.textContent = newScore.toString()
+}
+
+SCORE_ELEMENT.style.display = "block"
