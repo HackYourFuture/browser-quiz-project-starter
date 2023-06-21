fe33ab4392e993efe29cfbb73271e9ebbdcb3e3d
Hadelmur <murshed.hadeel@gmail.com>
2023-06-21 22:43:19 +0200

    last changes

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -44,3 +44,53 @@ const nextQuestion = () => {
   initQuestionPage();
 };
 
+import { ANSWER_BUTTONS } from '../constants.js';
+import { SCORE_ELEMENT } from '../constants.js';
+import { SCORE_BUTTON } from '../constants.js';
+import { SCORE_RESULT } from '../constants.js';
+// import { SCORE_NUM } from '../constants.js';
+
+let score = 0
+
+
+const ANSWER_BUTTONS = document.getElementById("answer-buttons")
+ANSWER_BUTTONS.forEach(button => {
+    button.addEventListener("click", checkAnswer)
+})
+
+const SCORE_BUTTON = document.getElementById("score-btn")
+SCORE_BUTTON.addEventListener("click", showScoreResult)
+
+function checkAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.classList.contains("correct")
+
+    if(isCorrect){
+        selectedBtn.innerHTML = "You got a point"
+        selectedBtn.classList.add("disabled")
+        score++
+        updateScore()
+    } else {
+        selectedBtn.innerHTML = "Sorry, no point"
+        selectedBtn.classList.add("disabled")
+    }
+}
+
+const SCORE_ELEMENT = document.getElementById("score")
+function updateScore(){
+  let score = parseInt(SCORE_ELEMENT.textContent);
+  let newScore = score + 1
+  SCORE_ELEMENT.textContent = newScore.toString()
+}
+
+const SCORE_RESULT = document.getElementById("score-result")
+function showScoreResult(){
+  SCORE_RESULT.innerHTML = updateScore()
+  // SCORE_ELEMENT.style.disply = "block"
+}
+
+
+
+
+
+
