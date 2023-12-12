2077d91b413ed5aa630473f3b37223588e5ccc5f
Erhan Koral <erhan.16k@gmail.com>
2023-12-12 19:02:54 +0100

    1_2 Retry Button Added

--- src/pages/resultPage.js
+++ src/pages/resultPage.js
@@ -1,5 +1,7 @@
-import { USER_INTERFACE_ID } from '../constants.js';
+import { RETRY_BUTTON_ID, USER_INTERFACE_ID } from '../constants.js';
 import { createResultElement } from '../views/resultView.js';
+import { quizData } from '../data.js';
+import { initQuestionPage } from './questionPage.js';
 
 export const initResultPage = () => {
   const element = document.getElementById(USER_INTERFACE_ID);
@@ -7,5 +9,14 @@ export const initResultPage = () => {
   const resultElement = createResultElement();
   element.appendChild(resultElement);
 
+  document
+    .getElementById(RETRY_BUTTON_ID)
+    .addEventListener('click', restartQuiz);
+
   return element;
+};
+
+const restartQuiz = () => {
+  quizData.currentQuestionIndex = 0;
+  initQuestionPage();
 };
--- src/views/resultView.js
+++ src/views/resultView.js
@@ -1,7 +1,10 @@
+import { RETRY_BUTTON_ID } from "../constants";
+
 export const createResultElement = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
 	  <h1>Thank you </h1>
+    <button id="${RETRY_BUTTON_ID}">RESTART</button>
 	`;
   return element;
 };
