2e83e444720ce084d2ef4cd996c72cb5a562cc32
Mariam Isaeva <mariam.hammad8@gmail.com>
2023-12-12 18:32:24 +0100

    moved vidBackground func to questionPage

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -1,36 +1,58 @@
 import {
-  ANSWERS_LIST_ID,
-  NEXT_QUESTION_BUTTON_ID,
-  USER_INTERFACE_ID,
+    ANSWERS_LIST_ID,
+    NEXT_QUESTION_BUTTON_ID,
+    USER_INTERFACE_ID,
 } from '../constants.js';
 import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
 
 export const initQuestionPage = () => {
-  const userInterface = document.getElementById(USER_INTERFACE_ID);
-  userInterface.innerHTML = '';
+    const userInterface = document.getElementById(USER_INTERFACE_ID);
+    userInterface.innerHTML = '';
 
-  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+    const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
 
-  const questionElement = createQuestionElement(currentQuestion.text);
+    const questionElement = createQuestionElement(currentQuestion.text);
 
-  userInterface.appendChild(questionElement);
+    userInterface.appendChild(questionElement);
 
-  const answersListElement = document.getElementById(ANSWERS_LIST_ID);
+    const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
-  for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
-    const answerElement = createAnswerElement(key, answerText);
-    answersListElement.appendChild(answerElement);
-  }
+    for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
+        const answerElement = createAnswerElement(key, answerText);
+        answersListElement.appendChild(answerElement);
+    }
 
-  document
-    .getElementById(NEXT_QUESTION_BUTTON_ID)
-    .addEventListener('click', nextQuestion);
+    document
+        .getElementById(NEXT_QUESTION_BUTTON_ID)
+        .addEventListener('click', nextQuestion);
+
+    vidBackground();
 };
+
 
 const nextQuestion = () => {
-  quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+    quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
 
-  initQuestionPage();
+    initQuestionPage();
 };
+
+
+//Add a video background 
+const vidBackground = () => {
+    //<video autoplay loop> 
+    //<source src=.. type=..> </vid>
+    const videoBG = document.createElement('video'); //<video>
+    videoBG.setAttribute('autoplay', true);
+    videoBG.setAttribute('loop', true);
+
+    const vidSource = document.createElement('source');
+    vidSource.setAttribute('src', '/assets/neon-light.mp4');
+    vidSource.setAttribute('type', 'video/mp4');
+
+    videoBG.appendChild(vidSource);
+    document.body.appendChild(videoBG);
+};
+
+
--- src/views/questionView.js
+++ src/views/questionView.js
@@ -8,25 +8,8 @@ import { NEXT_QUESTION_BUTTON_ID } from '../constants.js';
 
 export const createQuestionElement = (question) => {
     const element = document.createElement('div');
-
-    //============================
-    //Add a video background 
-    //<video autoplay loop> 
-    //<source src=.. type=..> </vid>
-    const videoBG = document.createElement('video'); //<video>
-    videoBG.setAttribute('autoplay', true);
-    videoBG.setAttribute('loop', true);
-
-    const vidSource = document.createElement('source');
-    vidSource.setAttribute('src', '/assets/neon-light.mp4');
-    vidSource.setAttribute('type', 'video/mp4');
-
-    videoBG.appendChild(vidSource);
-    document.body.appendChild(videoBG);
-    //================================
-
     // I use String.raw just to get fancy colors for the HTML in VS Code. 
-    //Added id in h1, class in ul
+    //Added id in h1
     element.innerHTML = String.raw` 
     <h1 id='questionHeader'>${question}</h1>
 
@@ -37,9 +20,6 @@ export const createQuestionElement = (question) => {
       Next question
     </button>
   `;
-
-
-
 
     return element;
 };
