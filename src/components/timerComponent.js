83c81956bdce21d1199d0838c84a34229b1e0361
Erhan Koral <erhan.16k@gmail.com>
2023-12-12 15:34:39 +0100

    2_7 Timer Component added

--- src/components/timerComponent.js
+++ src/components/timerComponent.js
@@ -1,10 +1,11 @@
-import { TIMER_COMPONENT_ID } from "../constants";
+import { TIMER_COMPONENT_ID } from '../constants';
 
 export const createTimerElement = (second) => {
-    const element = document.createElement('div');
-    element.id = TIMER_COMPONENT_ID;
-    element.innerHTML = String.raw`
-        <h1>Timer ${second} </h1>
+  const element = document.createElement('div');
+  element.id = TIMER_COMPONENT_ID;
+  element.className= 'timer-component'
+  element.innerHTML = String.raw`
+        <h1>${second} </h1>
       `;
-    return element
-  };
+  return element;
+};
