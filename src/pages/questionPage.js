13b472d7d4d2d247eae4bed2d7afaf311747cbc1
Nouran Mahmoud <nouran@miro.com>
2023-12-13 21:44:07 +0100

    convert from let to const

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -27,7 +27,7 @@ export const initQuestionPage = () => {
     .getElementById(NEXT_QUESTION_BUTTON_ID)
     .addEventListener('click', nextQuestion);
 
-  let timerElement = createTimerComponent(nextQuestion);
+  const timerElement = createTimerComponent(nextQuestion);
   questionElement.appendChild(timerElement);
 
   const timerInterval = setInterval(() => {
