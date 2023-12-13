eaa96a256942a4b3851f194d450f9757874ee942
Erhan Koral <erhan.16k@gmail.com>
2023-12-13 18:33:32 +0100

    bug occurs after merge fixed

--- src/components/resultComponent.js
+++ src/components/resultComponent.js
@@ -1,7 +1,10 @@
+import { RETRY_BUTTON_ID } from "../constants";
+
 export const createResultComponent = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
 	  <h1>Thank you </h1>
+    <button id="${RETRY_BUTTON_ID}">RETRY</button>
 	`;
   return element;
 };
