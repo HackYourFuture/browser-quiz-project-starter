0a987feea05969bd241927df807acf62891dc308
Erhan Koral <erhan.16k@gmail.com>
2023-12-13 13:21:41 +0100

    1_3 Disable choices after clicking once

--- src/components/answerComponent.js
+++ src/components/answerComponent.js
@@ -7,5 +7,10 @@ export const createAnswerComponent = (key, answerText) => {
   element.innerHTML = String.raw`
     ${key}: ${answerText};
   `;
+  element.addEventListener('click', disableClick)
   return element;
 };
+
+const disableClick = (e) => {
+  e.target.parentElement.style.cssText += 'pointer-events: none; opacity: 0.5';
+}
