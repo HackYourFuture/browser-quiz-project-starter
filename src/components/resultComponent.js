73002994888ddf1ea32b7291053f50b00f631f8d
Erhan Koral <erhan.16k@gmail.com>
2023-12-14 18:00:23 +0100

    resultComponent

--- src/components/resultComponent.js
+++ src/components/resultComponent.js
@@ -6,7 +6,7 @@ export const createResultComponent = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
 	  <h1>Thank you </h1>
-    <button id="${RETRY_BUTTON_ID}" onclick=${restartQuiz}>RETRY</button>
+    <button id="${RETRY_BUTTON_ID}" onclick="${restartQuiz}">RETRY</button>
 	`;
   return element;
 };
