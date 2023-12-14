b909b9386d2925003ea20b1171578988eec43f83
Erhan Koral <erhan.16k@gmail.com>
2023-12-14 09:26:31 +0100

    input radio and label to every answer

--- src/components/answerComponent.js
+++ src/components/answerComponent.js
@@ -5,12 +5,23 @@
 export const createAnswerComponent = (key, answerText) => {
   const element = document.createElement('li');
   element.innerHTML = String.raw`
-    ${key}: ${answerText};
+    <label for="${key}">
+      <input type="radio" name="answerItem" value=${key} id="${key}" />
+    ${answerText}
+    </label>
   `;
-  element.addEventListener('click', disableClick);
+  element.addEventListener('click', (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    disableClick()
+  });
   return element;
 };
-
-const disableClick = (e) => {
-  e.target.parentElement.style.cssText += 'pointer-events: none; opacity: 0.5';
+const disableClick = () => {
+  const answerList = document.querySelectorAll('li [name="answerItem"]');
+  answerList.forEach(element => {
+    element.disabled = true;
+    element.parentElement.parentElement.style.cssText += 'opacity: 0.5';
+  })
 };
+
