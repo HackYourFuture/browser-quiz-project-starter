db4f22c1642974ba767f55fc7759b5a545aeae8b
Erhan Koral <erhan.16k@gmail.com>
2023-12-14 12:25:23 +0100

    cannot call restartQuiz function

--- src/components/resultComponent.js
+++ src/components/resultComponent.js
@@ -1,10 +1,17 @@
 import { RETRY_BUTTON_ID } from "../constants";
+import { quizData } from '../data.js';
+import { initQuestionPage } from '../pages/questionPage.js';
 
 export const createResultComponent = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
 	  <h1>Thank you </h1>
-    <button id="${RETRY_BUTTON_ID}">RETRY</button>
+    <button id="${RETRY_BUTTON_ID}" onclick=${restartQuiz}>RETRY</button>
 	`;
   return element;
+};
+
+const restartQuiz = () => {
+  quizData.currentQuestionIndex = 0;
+  initQuestionPage();
 };
--- src/pages/resultPage.js
+++ src/pages/resultPage.js
@@ -1,7 +1,5 @@
-import { RETRY_BUTTON_ID, USER_INTERFACE_ID } from '../constants.js';
+import { USER_INTERFACE_ID } from '../constants.js';
 import { createResultComponent } from '../components/resultComponent.js';
-import { quizData } from '../data.js';
-import { initQuestionPage } from './questionPage.js';
 
 export const initResultPage = () => {
   const element = document.getElementById(USER_INTERFACE_ID);
@@ -9,14 +7,8 @@ export const initResultPage = () => {
   const resultElement = createResultComponent();
   element.appendChild(resultElement);
 
-  document
-    .getElementById(RETRY_BUTTON_ID)
-    .addEventListener('click', restartQuiz);
 
   return element;
 };
 
-const restartQuiz = () => {
-  quizData.currentQuestionIndex = 0;
-  initQuestionPage();
-};
+
