d271c1b9b19b7a0525551fddc05ed949485d0c9c
burhan1997 <burhanelaldi97@gmail.com>
2023-12-14 15:11:01 +0100

    reset button added

--- src/pages/endPage.js
+++ src/pages/endPage.js
@@ -1,5 +1,6 @@
 import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID } from '../constants.js';
 import { createEndElement } from '../views/endView.js';
+import { initWelcomePage } from './welcomePage.js';
 
 
 export const initEndPage = () => {
@@ -7,8 +8,16 @@ export const initEndPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
 
-  const endElement = createEndElement(); //This function is responsible for creating the message of the end page
+ //restartQuiz function as a callback to createEndElement
+  const endElement = createEndElement(restartQuiz);
   userInterface.appendChild(endElement);
 };
 
-//Converted to the same codes from welcomePage :)
+// Callback function to restart the quiz
+const restartQuiz = () => {
+  // Resetting quiz data
+  quizData.currentQuestionIndex = 0;
+  quizData.quizCompleted = false;
+  console.log('Restarting the quiz...');
+  initWelcomePage(); // Go back to the welcome page
+};
--- src/views/endView.js
+++ src/views/endView.js
@@ -1,9 +1,16 @@
-export const createEndElement = () => {
-  // Create a new HTML div element
+export const createEndElement = (restartCallback) => { 
+  //Create a new HTML div element and with innerHTML set the congrats message and restart button
   const element = document.createElement('div');
   element.innerHTML = `
   <h1>Congratulations! You've completed the quiz!</h1>
   <p>Thank you for participating.</p>
+  <button id="restart-quiz-button">Restart Quiz</button>
 `;
+
+const restartButton = element.querySelector('#restart-quiz-button');
+  if (restartButton) {
+    restartButton.addEventListener('click', restartCallback);
+  }
+
   return element;
 };
