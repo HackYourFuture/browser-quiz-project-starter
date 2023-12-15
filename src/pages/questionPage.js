22bfeb72e4f3dd2715c964bc47ed77b67f8c6431
Ali Othman <alioth840@gmail.com>
2023-12-15 11:14:21 +0100

    I have created function that counts the right answers also i created some animation and some general enhancement

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -8,6 +8,8 @@ import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
 import { initEndPage } from './endPage.js'; //importing initEndPage 
 
+let videoLoaded = false;
+
 export const initQuestionPage = () => {
     const userInterface = document.getElementById(USER_INTERFACE_ID);
     userInterface.innerHTML = '';
@@ -20,6 +22,10 @@ export const initQuestionPage = () => {
 
     const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
+    let selectedAnswer = null; // Added variable to store the selected answer
+    let myImg1 = document.querySelector(".img-1");
+    let myImg2 = document.querySelector(".img-2");
+
     for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
         const answerElement = createAnswerElement(key, answerText); //need this also
 
@@ -29,23 +35,30 @@ export const initQuestionPage = () => {
 
         //Add an eventListener here for checking the answers
         answersListElement.addEventListener('click', (e) => {
+            if(selectedAnswer === null){
 
-            const clickedAnswer = e.target.getAttribute('data-answer');
-            const index = Array.from(answersListElement.children).indexOf(e.target);
+                const clickedAnswer = e.target.getAttribute('data-answer');
+                const index = Array.from(answersListElement.children).indexOf(e.target);
 
-            if (clickedAnswer === currentQuestion.correct) {
+                if (clickedAnswer === currentQuestion.correct) {
+                    quizData.correctAnswersCount++
+                    myImg1.src = "https://cliply.co/wp-content/uploads/2021/09/CLIPLY_372109170_FREE_FIREWORKS_400.gif";
+                    myImg1.style.display = "block"
+                    myImg2.src = "https://cliply.co/wp-content/uploads/2021/09/CLIPLY_372109170_FREE_FIREWORKS_400.gif";
+                    myImg2.style.display = "block"
+                    answersListElement.children[index].style.boxShadow = '0 0 10px 10px #00FF00';
+                    answersListElement.children[index].style.transition = 'none';
+                } else {
 
-                answersListElement.children[index].style.boxShadow = '0 0 10px 10px #00FF00';
-                answersListElement.children[index].style.transition = 'none';
-            } else {
+                    answersListElement.children[index].style.boxShadow = '0 0 10px 10px #FF0000';
 
-                answersListElement.children[index].style.boxShadow = '0 0 10px 10px #FF0000';
+                    const correctAnswer = document.querySelector(`[data-answer="${currentQuestion.correct}"]`);
+                    correctAnswer.style.boxShadow = '0 0 10px 10px #00FF00';
+                }
 
-                const correctAnswer = document.querySelector(`[data-answer="${currentQuestion.correct}"]`);
-                correctAnswer.style.boxShadow = '0 0 10px 10px #00FF00';
+                disableClick(); //define it Don't forget
+                selectedAnswer = clickedAnswer
             }
-
-            disableClick(); //define it Don't forget
         });
         //========================
         answersListElement.appendChild(answerElement);
@@ -89,6 +102,7 @@ const nextQuestion = () => {
 
 //Add a video background 
 const vidBackground = () => {
+    if(!videoLoaded){
     //<video autoplay loop> 
     //<source src=.. type=..> </vid>
     const videoBG = document.createElement('video'); //<video>
@@ -101,6 +115,8 @@ const vidBackground = () => {
 
     videoBG.appendChild(vidSource);
     document.body.appendChild(videoBG);
+    videoLoaded = true ;
+    }
 };
 
 
