943984c48949a9f3d935765ee19a07a4611faca6
Mariam Isaeva <mariam.hammad8@gmail.com>
2023-12-17 19:45:52 +0100

    Added mediaQuery , fixed default controls in phone

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -19,7 +19,7 @@ const randomizeQuestions = (qArray) => {
 };
 
 export const initQuestionPage = () => {
-    
+
     randomizeQuestions(quizData.questions);
 
     const userInterface = document.getElementById(USER_INTERFACE_ID);
@@ -45,35 +45,35 @@ export const initQuestionPage = () => {
         answerElement.setAttribute('data-answer', key);
 
         //Add an eventListener here for checking the answers
-       answersListElement.addEventListener('click', (e) => {
-        if (selectedAnswer === null) {
-        const clickedAnswer = e.target.getAttribute('data-answer');
-        const index = Array.from(answersListElement.children).indexOf(e.target);
+        answersListElement.addEventListener('click', (e) => {
+            if (selectedAnswer === null) {
+                const clickedAnswer = e.target.getAttribute('data-answer');
+                const index = Array.from(answersListElement.children).indexOf(e.target);
 
-        //The error you're encountering indicates that the answersListElement.children[index] is returning undefined at some point. This could happen if the index value is not within the valid range of children elements in your answersListElement.
-        if (index !== -1) {
-            if (clickedAnswer === currentQuestion.correct) {
-                quizData.correctAnswersCount++;
-                myImg1.src = "https://cliply.co/wp-content/uploads/2021/09/CLIPLY_372109170_FREE_FIREWORKS_400.gif";
-                myImg1.style.display = "block"
-                myImg2.src = "https://cliply.co/wp-content/uploads/2021/09/CLIPLY_372109170_FREE_FIREWORKS_400.gif";
-                myImg2.style.display = "block"
-                answersListElement.children[index].style.boxShadow = '0 0 10px 10px #00FF00';
-                answersListElement.children[index].style.transition = 'none';
-            } else {
-                answersListElement.children[index].style.boxShadow = '0 0 10px 10px #FF0000';
+                //The error you're encountering indicates that the answersListElement.children[index] is returning undefined at some point. This could happen if the index value is not within the valid range of children elements in your answersListElement.
+                if (index !== -1) {
+                    if (clickedAnswer === currentQuestion.correct) {
+                        quizData.correctAnswersCount++;
+                        myImg1.src = "https://cliply.co/wp-content/uploads/2021/09/CLIPLY_372109170_FREE_FIREWORKS_400.gif";
+                        myImg1.style.display = "block"
+                        myImg2.src = "https://cliply.co/wp-content/uploads/2021/09/CLIPLY_372109170_FREE_FIREWORKS_400.gif";
+                        myImg2.style.display = "block"
+                        answersListElement.children[index].style.boxShadow = '0 0 10px 10px #00FF00';
+                        answersListElement.children[index].style.transition = 'none';
+                    } else {
+                        answersListElement.children[index].style.boxShadow = '0 0 10px 10px #FF0000';
 
-                const correctAnswer = document.querySelector(`[data-answer="${currentQuestion.correct}"]`);
-                if (correctAnswer) {
-                    correctAnswer.style.boxShadow = '0 0 10px 10px #00FF00';
+                        const correctAnswer = document.querySelector(`[data-answer="${currentQuestion.correct}"]`);
+                        if (correctAnswer) {
+                            correctAnswer.style.boxShadow = '0 0 10px 10px #00FF00';
+                        }
+                    }
+
+                    disableClick();
+                    selectedAnswer = clickedAnswer;
                 }
             }
-
-            disableClick();
-            selectedAnswer = clickedAnswer;
-        }
-    }
-});
+        });
 
         //========================
         answersListElement.appendChild(answerElement);
@@ -132,6 +132,13 @@ const vidBackground = () => {
         vidSource.setAttribute('src', '/assets/neon-light.mp4');
         vidSource.setAttribute('type', 'video/mp4');
 
+        //===to remove default controls when we open from phone
+        if ('playsInline' in videoBG) {
+            videoBG.playsInline = true;
+        } else {
+            videoBG.setAttribute('playsinline', '');
+        }
+        //======
         videoBG.appendChild(vidSource);
         document.body.appendChild(videoBG);
         videoLoaded = true;
@@ -182,11 +189,11 @@ function quizTimer(duration, count) {
         myTimer.innerHTML = `${minutes}:${seconds}`;
         document.querySelector(".quiz-timer").style.display = "block"
 
-    if (--duration < 0) {
-        clearInterval(countDown);
-        nextQuestion();
-    }
-}, 1000);
+        if (--duration < 0) {
+            clearInterval(countDown);
+            nextQuestion();
+        }
+    }, 1000);
 }
 
 export const stopTimer = () => {
