5def0b6259c050803d2982362e57eb052587aac8
tenzijn <tenzinkunchok2022@gmail.com>
2023-12-17 11:53:53 +0100

    added fireworks

--- src/utils/checkAnswer.js
+++ src/utils/checkAnswer.js
@@ -6,11 +6,15 @@ export const checkAnswer = (questionIndex) => {
   const correctAns = quizData.questions[questionIndex].correct;
   const answerList = document.querySelectorAll('li');
 
+  const fireworkBefore = document.getElementById('firework_before');
+  const fireworkAfter = document.getElementById('firework_after');
   if (userCurrentSelected == correctAns) {
     answerList.forEach((answer) => {
       if (answer.id == userCurrentSelected) {
         answer.classList.add(CORRECT);
         quizData.totalScore += 10;
+        fireworkAfter.classList.add('after');
+        fireworkBefore.classList.add('before');
       }
     });
   } else {
